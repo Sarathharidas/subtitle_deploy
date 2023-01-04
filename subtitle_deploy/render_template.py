e1fcbcbce97ad6cba0ee9faa687afcc2381c24a8
Sarathharidas <30758858+Sarathharidas@users.noreply.github.com>
2023-01-03 23:33:35 -0500

    Update render_template.py

--- subtitle_deploy/render_template.py
+++ subtitle_deploy/render_template.py
@@ -65,7 +65,8 @@ def youtube():
   youtube_link = request.form.get('youtube_link')
   with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([youtube_link])
-  whisper_api(new_video.wav, youtube_link)
+  srtFilename = whisper_api(new_video.wav, youtube_link)
+  os.system( 'rm new_video.wav')
   return send_file(srtFilename, as_attachment=True)
 
 def whisper_api(audio_wav, youtube_link):
